6786352f98f84f7ace78dc74c6398bd81f0aff9e
unknown <leyluw_777@mail.ru>
2020-02-01 13:59:30 +0400

    First repo

---
+++ js/java.js
@@ -0,0 +1,29 @@
+
+
+// var fruits= ["banana", "lemon", "apple"]
+// document.write(fruits)
+
+// function myFunction(){
+// 	fruits.push ("Mango");
+// 	document.write(fruits)
+// }
+
+// var fruits = ["Mango ", "Apple " , "Lemon"]
+// document.write(fruits + " <br> ")
+
+// document.write("The length of array is " + fruits.length + ".")
+// function myFunction(){
+// 	fruits.push ("Orange");
+// 	document.getElementById("demo").innerHTML = fruits;
+// }
+
+function clickme(){
+	var a=parseInt(document.getElementById("a").value)
+	var b=parseInt(document.getElementById("b").value)
+	var demo=document.getElementById("demo")
+}
+
+for (var i=a; i<=b; i++) {
+	document.write(<p> + i + <p/>)
+}
+
